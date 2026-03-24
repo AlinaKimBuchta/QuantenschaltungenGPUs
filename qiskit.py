c14400eb5a3f3d03fa34af40c2f48a8c4ee5f17a
Alina <akibu06@gmail.com>
2026-03-24 15:59:00 +0100

    qiskit

---
+++ qiskit.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
+from qiskit.visualization import plot_histogram
+
+qc = QuantumCircuit(4)
+
+qc.h(0)
+qc.cx(0, 1)
+qc.cx(1, 2)
+qc.cx(2, 3)
+qc.measure_all()
+
+
+sampler = StatevectorSampler()
+result = sampler.run([qc], shots=1024).result()
+print(result[0].data.meas.get_counts())
+
+
+counts = result[0].data.meas.get_counts()
+plot_histogram(counts)
+
+qc.draw()
+plt.show()
